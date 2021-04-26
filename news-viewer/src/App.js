ccebe565375ebd6a3330bce79f3008323464fbb3
smith <ksjand1004@gmail.com>
2021-04-26 22:39:35 +0900

    New half working

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,34 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { setDefaultHandler } from '../node_modules/workbox-routing/setDefaultHandler';
+import React from 'react';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-    } catch (e) {
-      console.log(e);
-    }
-    axios
-      .get('http://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setData(response.data);
-      });
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
